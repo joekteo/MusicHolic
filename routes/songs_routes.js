914455d49218bfc8913be817bc1f9979cc7a6f06
Joe Teo <revelate@live.com>
2014-11-21 12:55:25 -0800

    fix jwt

--- routes/songs_routes.js
+++ routes/songs_routes.js
@@ -1,13 +1,11 @@
-/*jshint node: true*/
 'use strict';
 var Url = require('../models/url');
 var score = require('../lib/alg');
 var express = require('express');
 var request = require('superagent');
 var bodyParser = require('body-parser');
-var async = require('async');
 var app = express();
-
+var async = require('async');
 app.use(bodyParser.json());
 
 module.exports = function(app) {
@@ -17,6 +15,7 @@ module.exports = function(app) {
   });
 
   app.get('/api', function(req, res) {
+    console.log(req.body.url);
     Url.findOne({'info.url': req.body.url}, function(err, data) {
       if (err) return res.status(500).send('there was an error');
       res.send({url: data.info.url});
@@ -25,18 +24,15 @@ module.exports = function(app) {
 
   app.post('/api', function(req, res) {
     var songId;
-
     async.series([
-
       function(callback) {
         request(req.body.url)
-          .end(function(req, sData) {
-            var parsedData = JSON.parse(sData.text);
-            songId = parsedData.response.songs[0].id;
-          });
-        callback(null, 'one');
+        .end(function(req, sData) {
+          var parsedData = JSON.parse(sData.text);
+          songId = parsedData.response.songs[0].id;
+          callback(null, 'one');
+        });
       },
-
       function(callback) {
         var key = process.env.ECHO_KEY || 'PGTZEGJKHLCVM1ADB';
         var newUrl = 'http://developer.echonest.com/api/v4/song/profile?' +
@@ -45,25 +41,26 @@ module.exports = function(app) {
         '&id=' +
         songId +
         '&bucket=audio_summary&format=json';
-
+        console.log(newUrl);
         request(newUrl)
-          .end(function(req, echoData) {
-            var parsedData = JSON.parse(echoData.text);
-            var danceability = parsedData.response.songs[0].audio_summary.
-            danceability;
-            var valence = parsedData.response.songs[0].audio_summary.valence;
-            var energy = parsedData.response.songs[0].audio_summary.energy;
-            var songScore = (danceability + energy + valence);
+         .end(function(req, echoData) {
+           var parsedData = JSON.parse(echoData.text);
+           console.log(parsedData);
+           var danceability = parsedData.response.songs[0].audio_summary.
+           danceability;
+           var valence = parsedData.response.songs[0].audio_summary.valence;
+           var energy = parsedData.response.songs[0].audio_summary.energy;
 
-            score(songScore);
-            res.json(score(songScore));
-          });
+           var songScore = (danceability + energy + valence);
+           score(songScore);
+           console.log(score(songScore));
+           res.json(score(songScore));
+         });
         callback(null, 'two');
       }
     ],
-
       function(err, results) {
-        return results;
+        console.log(results);
       });
   });
 };
