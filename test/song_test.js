dead1849d502b118b9451118c754c92eeba47dae
Joe Teo <revelate@live.com>
2014-11-24 12:01:28 -0800

    travis.yml

--- test/song_test.js
+++ test/song_test.js
@@ -21,10 +21,6 @@ describe('song test', function() {
       url:'http://developer.echonest.com/api/v4/song/search?' +
       'api_key=' +
       'PGTZEGJKHLCVM1ADB&artist=the%20used&title=empty%20with%20you'
-      //Echo Nest limits accesses to 2 songs per minute.
-      //Test will return 'Cannot read property '0' of undefined' if over limit
-      //Comment out line 23 and uncomment line 27 if limit is over
-      //'PGTZEGJKHLCVM1ADB&artist=kanye%20west&title=all%20of%20the%20lights'
     }).
     end(function(err, res) {
       expect(err).to.eql(null);
